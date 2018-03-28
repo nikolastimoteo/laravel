ea15839a015dce98770257784a162ec4e48f13e4
unknown <nikolastps@hotmail.com>
2018-03-27 22:36:58 -0300

    Implementação de User e UserSocial

---
+++ app/Entities/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use SoftDeletes;
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'cpf', 'name', 'phone', 'birth', 'gender', 'notes', 'email', 'password', 'status', 'permission'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
